bdcb6f235e07b52c527362a24e4b518642e03e94
Adam Bien <abien@adam-bien.com>
2020-12-10 13:00:50 +0100

    openapi implemented

--- santa/store/src/main/java/airhacks/gifts/entity/Gift.java
+++ santa/store/src/main/java/airhacks/gifts/entity/Gift.java
@@ -1,11 +1,19 @@
 package airhacks.gifts.entity;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 public class Gift {
 
+    @Schema(readOnly = true)
+    public long id;
+    @Schema(example = "keyboard")
     public String name;
+
+    @Schema(example="13")
     public int price;
     
     public Gift(String name, int price) {
+        this.id = System.currentTimeMillis();
         this.name = name;
         this.price = price;
     }
