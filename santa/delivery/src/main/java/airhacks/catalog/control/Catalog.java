7f362f5814d9827beacfd12e5c58f13d2b6da629
Adam Bien <abien@adam-bien.com>
2020-12-10 13:34:42 +0100

    rest client timeout -> chris is happy

--- santa/delivery/src/main/java/airhacks/catalog/control/Catalog.java
+++ santa/delivery/src/main/java/airhacks/catalog/control/Catalog.java
@@ -2,6 +2,7 @@ package airhacks.catalog.control;
 
 import javax.inject.Inject;
 
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.metrics.annotation.SimplyTimed;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
@@ -14,9 +15,14 @@ public class Catalog {
     @RestClient
     CatalogResourceClient client;
 
+    @Fallback(fallbackMethod = "tooSlow")
     @SimplyTimed(tags = "layer=control")
     public String catalog() {
         var name = this.client.catalog();
         return name.getString("name", "nothing");
+    }
+
+    public String tooSlow() {
+        return "santa is too slow";
     }
 }
